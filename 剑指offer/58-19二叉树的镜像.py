ec59e3f9fb4336c7f1a59fa23b9bcfe0de608933
Jeff <jz3766@nyu.edu>
2021-02-20 05:43:41 -0500

    Update 58-19二叉树的镜像.py

--- 剑指offer/58-19二叉树的镜像.py
+++ 剑指offer/58-19二叉树的镜像.py
@@ -16,6 +16,24 @@ class Solution:
         self.BSTMirror(root.rightChild)
         return
 
+'''
+判断二叉树是不是对称的。如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+'''
+class Solution:
+    def isSymmetrical(self, root):
+        if root == None:
+            return True
+        return self.isBalance(root.leftChild, root.rightChild)
+
+    def isBalance(self, left, right):
+        if (left==None)and(right==None):      # reach to the leafnode
+            return True
+        if ((left==None)and(right!=None))or((left!=None)and(right==None)):
+            return False
+        if left.val != right.val:
+            return False
+        return self.isBalance(left.rightChild,right.leftChild) and self.isBalance(left.leftChild,right.rightChild)
+
 
 # build the BST
 tree = Tree()
