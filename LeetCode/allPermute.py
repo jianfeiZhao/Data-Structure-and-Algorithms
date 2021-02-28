8713c08e051ce39e292f447efb388a9e7c2cc04c
Jeff <jz3766@nyu.edu>
2021-02-28 00:25:15 -0500

    Update allPermute.py

--- LeetCode/allPermute.py
+++ LeetCode/allPermute.py
@@ -3,27 +3,31 @@
 
 在递归之前做出选择，在递归之后撤销刚才的选择，就能正确得到每个节点的选择列表和路径。
 '''
-res = []
-def permute(nums):  
-    track = []
-    backtrack(nums, track)
-    return res
+class Solution:
+    def permute(self, nums):  
+        res = []
+        track = []
 
-def backtrack(nums, track):
-    # trigger end condition
-    if len(track) == len(nums):
-        res.append(track)
-        return
+        def backtrack(nums, track, res):
+            # trigger end condition
+            if len(track) == len(nums):
+                print(track)
+                res.append(track)
+                return
 
-    for i in range(len(nums)):
-        if nums[i] in track:
-            continue
-        track.append(nums[i])
-        # next choice
-        backtrack(nums, track)
-        # go back to make another choice, remove current choice
-        track.pop()
+            for i in range(len(nums)):
+                if nums[i] in track:
+                    continue
+                track.append(nums[i])
+                # next choice
+                backtrack(nums, track, res)
+                # go back to make another choice, remove current choice
+                track.pop()
 
+        backtrack(nums, track, res)
+        #print(res)
+        return res
 
-data = [1,2,3,4]
-print(permute(data))
+s = Solution()
+data = [1,2,3]
+print(s.permute(data))
