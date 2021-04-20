90b03ad97ddaddd052eea9de9610fc6043814d8a
Jeff <jz3766@nyu.edu>
2021-04-20 10:45:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 inp = input().split('\n')
 for line in inp:
     n, m = line.split(' ')
@@ -5,3 +6,19 @@ for line in inp:
     for i in range(m):
         res = n + round(n**0.5, 2)
     print(round(res,2))
+'''
+class Solution:
+    def findValue(self, nums):
+        if not nums: return None
+        nums = sorted(nums)
+        
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return len(nums)
+
+arr1 = [0,1,2,4]
+arr2 = [0,1,2]
+s = Solution()
+print(s.findValue(arr1))   # 3
+print(s.findValue(arr2))   # 3
